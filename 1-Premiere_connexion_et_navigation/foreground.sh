8e698cccf7706c33793b0c8585c337523e3cd3ab
H3xano <abdou0xdz@gmail.com>
2025-10-06 02:55:45 +0200

    Updated foreground.sh

--- 1-Premiere_connexion_et_navigation/foreground.sh
+++ 1-Premiere_connexion_et_navigation/foreground.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# Changer d'utilisateur
+su - learner
+
+# Changer de directory
+cd ~
+
 # Exécuter le banner
 if [ -f /tmp/banner.sh ]; then
   bash /tmp/banner.sh
 else
   echo "Banner non trouvé (/tmp/banner.sh manquant)"
 fi
-
-# Changer d'utilisateur
-su - learner
-
-# Changer de directory
-cd ~
