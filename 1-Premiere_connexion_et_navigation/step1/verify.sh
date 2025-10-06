be9eedd1426373c99aa6e4d3caa352d236a6d149
H3xano <abdou0xdz@gmail.com>
2025-10-06 03:36:54 +0200

    Updated verify.sh

--- 1-Premiere_connexion_et_navigation/step1/verify.sh
+++ 1-Premiere_connexion_et_navigation/step1/verify.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-set -e
 
-if ! grep -q "whoami" ~/.bash_history; then exit 1; fi
-if ! grep -q "id" ~/.bash_history; then exit 1; fi
-if ! grep -q "groups" ~/.bash_history; then exit 1; fi
+history | grep -q "whoami"
+history | grep -q "id"
+history | grep -q "groups"
