f700634413431381869a27b18add7d510b1b49a7
H3xano <abdou0xdz@gmail.com>
2025-10-06 03:44:27 +0200

    Updated verify.sh

--- 1-Premiere_connexion_et_navigation/step1/verify.sh
+++ 1-Premiere_connexion_et_navigation/step1/verify.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-history | grep -q "whoami"
-history | grep -q "id"
-history | grep -q "groups"
+! history | grep -q "whoami"
+! history | grep -q "id"
+! history | grep -q "groups"
