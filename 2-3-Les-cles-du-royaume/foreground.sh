37cb3a7bf76e927ca4b1368a3cdec237f8a5ba4d
CyberNomad <abdou0xdz@gmail.com>
2025-10-14 18:35:03 +0200

    Added necessary scripts

---
+++ 2-3-Les-cles-du-royaume/foreground.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+su - learner
+
+cd ~
+
+if [ -f /tmp/banner.sh ]; then
+  bash /tmp/banner.sh
+else
+  echo "Banner non trouvé (/tmp/banner.sh manquant)"
+fi
