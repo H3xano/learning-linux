67b43e0d3d6135c2eaa88309376b70a1fe548015
CyberNomad <abdou0xdz@gmail.com>
2025-10-15 17:02:35 +0200

    Updated foreground.sh for lab 3-1

--- 3-1/foreground.sh
+++ 3-1/foreground.sh
@@ -1,21 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# --- Lancement du simulateur de log en arrière-plan ---
-# Ce script simule un fichier de log qui se remplit en temps réel.
-(
-  count=0
-  while true; do
-    sleep 3
-    count=$((count+1))
-    echo "$(date): INFO - Événement normal N°$count." >> /home/learner/live_stream.log
-    sleep 2
-    if [ $(($count % 2)) -eq 0 ]; then
-      echo "$(date): ERROR - Une erreur simulée est survenue !" >> /home/learner/live_stream.log
-    fi
-  done
-) &
-
 # --- Passage à l'utilisateur 'learner' et affichage de la bannière ---
 # Le 'su - learner' est essentiel pour charger l'environnement de l'apprenant
 # et exécuter le .bashrc
