8c7da5a805dee5941df6edd460d868cf2abb1500
CyberNomad <abdou0xdz@gmail.com>
2025-10-15 17:05:18 +0200

    Updated foreground.sh for lab 3-1

--- 3-1/foreground.sh
+++ 3-1/foreground.sh
@@ -4,4 +4,6 @@ set -euo pipefail
 # --- Passage à l'utilisateur 'learner' et affichage de la bannière ---
 # Le 'su - learner' est essentiel pour charger l'environnement de l'apprenant
 # et exécuter le .bashrc
-su - learner -c 'cd ~ && bash /tmp/banner.sh && exec bash'
+su - learner
+cd ~ 
+bash /tmp/banner.sh
