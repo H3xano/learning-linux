1a900819264257a1b99a3ac6f52a202144d47ad4
CyberNomad <abdou0xdz@gmail.com>
2025-10-19 17:10:26 +0200

    Fixed an issue in step4 for lab 4-5

--- 4-5/step3/verify.sh
+++ 4-5/step3/verify.sh
@@ -4,7 +4,7 @@ FILES=("$HOME/.bash_history" "/home/learner/.bash_history")
 found() { local p="$1"; for f in "${FILES[@]}"; do [ -f "$f" ] && grep -q "$p" "$f" && return 0; done; return 1; }
 
 found 'locate sshd_config' || exit 1
-found 'touch /tmp/mon_fichier_tout_neuf.txt' || exit 1
+found 'touch ~/mon_fichier_tout_neuf.txt' || exit 1
 found 'sudo updatedb' || exit 1
 found 'locate mon_fichier_tout_neuf.txt' || exit 1
 
