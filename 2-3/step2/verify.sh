abf45317127b25ecb9dacb98188cb5b6f7e95871
CyberNomad <abdou0xdz@gmail.com>
2025-10-22 19:19:44 +0200

    Updated verify.sh step2 lab2-3

--- 2-3/step2/verify.sh
+++ 2-3/step2/verify.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-set -e; FILES=("$HOME/.bash_history" "/home/learner/.bash_history"); found() { local p="$1"; for f in "${FILES[@]}"; do [ -f "$f" ] && grep -q "$p" "$f" && return 0; done; return 1; }
+set -e
+FILES=("$HOME/.bash_history" "/home/learner/.bash_history")
+found() { local p="$1"; for f in "${FILES[@]}"; do [ -f "$f" ] && grep -q "$p" "$f" && return 0; done; return 1; }
 found 'sudo cat /etc/shadow' || exit 1
 found 'sudo -l' || exit 1
 found 'sudo -i' || exit 1
