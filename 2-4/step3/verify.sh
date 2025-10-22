fae5faab1a4d3a72026571de7f7eff85a3be70b9
CyberNomad <abdou0xdz@gmail.com>
2025-10-22 19:51:30 +0200

    Updated verify.sh step3 lab2-4

--- 2-4/step3/verify.sh
+++ 2-4/step3/verify.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -e
-# Vérifie que le PS1 a été ajouté au .bashrc
-if ! grep -q 'PS1="${GREEN}\u@\h${RESET}' /home/learner/.bashrc; then
-    exit 1
-fi
-FILES=("$HOME/.bash_history" "/home/learner/.bash_history"); found() { local p="$1"; for f in "${FILES[@]}"; do [ -f "$f" ] && grep -q "$p" "$f" && return 0; done; return 1; }
-found '^cd /etc$' || exit 1
+FILES=("$HOME/.bash_history" "/home/learner/.bash_history")
+found() { local p="$1"; for f in "${FILES[@]}"; do [ -f "$f" ] && grep -q "$p" "$f" && return 0; done; return 1; }
+
+# Vérifie que les 3 commandes essentielles ont été utilisées
+found 'source ~/.bashrc' || exit 1
+found 'cd /etc' || exit 1
+
 echo -n "done"
