17ba7e8799d23154f81517a6db63b1c9e5fb9176
CyberNomad <abdou0xdz@gmail.com>
2025-10-22 19:23:18 +0200

    Updated verify.sh step2 lab2-3

--- 2-3/step2/verify.sh
+++ 2-3/step2/verify.sh
@@ -5,5 +5,4 @@ found() { local p="$1"; for f in "${FILES[@]}"; do [ -f "$f" ] && grep -q "$p" "
 found 'sudo cat /etc/shadow' || exit 1
 found 'sudo -l' || exit 1
 found 'sudo -i' || exit 1
-if [[ $(whoami) != "learner" ]]; then exit 1; fi
 echo -n "done"
