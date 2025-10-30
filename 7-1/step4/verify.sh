df93b89065d76bfa426376e424aa5983467f7ef3
CyberNomad <abdou0xdz@gmail.com>
2025-10-30 19:24:16 +0100

    Updated step4 for lab7-1

--- 7-1/step4/verify.sh
+++ 7-1/step4/verify.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
-[ -f script_condition.sh ] || exit 1
 
 FILES=("$HOME/.bash_history" "/home/learner/.bash_history")
 found() { local p="$1"; for f in "${FILES[@]}"; do [ -f "$f" ] && grep -q "$p" "$f" && return 0; done; return 1; }
