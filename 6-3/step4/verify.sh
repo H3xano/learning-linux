50d0759a86fb5efd98cf1faa07d13cd8b4926e05
CyberNomad <abdou0xdz@gmail.com>
2025-10-30 18:39:53 +0100

    Fixed an issue with verification in lab6-3

--- 6-3/step4/verify.sh
+++ 6-3/step4/verify.sh
@@ -2,7 +2,7 @@
 set -e; FILES=("$HOME/.bash_history" "/home/learner/.bash_history"); found() { local p="$1"; for f in "${FILES[@]}"; do [ -f "$f" ] && grep -q "$p" "$f" && return 0; done; return 1; }
 
 # Vérifie l'activation du SGID
-found 'chmod g\+s shared_folder' || exit 1
+found 'chmod g+s shared_folder' || exit 1
 
 # Vérifie la création du fichier pour tester l'héritage
 found 'touch shared_folder/rapport_equipe.txt' || exit 1
